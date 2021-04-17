607d3dcf832f11d5d1542cea8b215bb2ec0ab197
Raj Bhatta <bhattaraj21@gmail.com>
2021-04-17 13:13:38 -0700

    String interpolation example

---
+++ 04-Display-Data-Respond-event/4.2 String-Interpolation/src/components/Hero-add.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h3>Add Hero </h3>
+
+        <form action="/action_page.php">
+        <label for="fname">First name:</label><br>
+        <input type="text" id="fname" name="fname"><br>
+
+        <label for="lname">Last name:</label><br>
+        <input type="text" id="lname" name="lname"><br><br>
+
+        <input type="button" value="Submit"> &nbsp; &nbsp;
+         <input type="button" value="Clear">
+        </form>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'HeroAdd'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- 04-Display-Data-Respond-event/4.2 String-Interpolation/src/components/Heros.vue
+++ 04-Display-Data-Respond-event/4.2 String-Interpolation/src/components/Heros.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <Header></Header>
+        <HeroAdd></HeroAdd>
         <HeroDetail></HeroDetail>
         <HeroList></HeroList>
     </div>
@@ -8,12 +9,13 @@
 
 <script>
 import Header from './Header.vue'
+import HeroAdd from '@/components/Hero-add.vue'
 import HeroDetail from './Hero-detail.vue'
 import HeroList from './Hero-list.vue'
 
     export default {
         name:'Heros',
-        components:{Header,HeroDetail,HeroList}
+        components:{Header,HeroAdd,HeroDetail,HeroList}
     }
 </script>
 
