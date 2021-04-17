b2bc9a45ae1eeb7ba51b7cf4ddfe76ce8290c43a
Raj Bhatta <bhattaraj21@gmail.com>
2021-04-17 13:42:34 -0700

    Example of two way data binding

---
+++ 04-Display-Data-Respond-event/4.4 Two-way-data-binding/src/components/Heros.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Header></Header>
+        <HeroAdd></HeroAdd>
+        <HeroDetail></HeroDetail>
+        <HeroList></HeroList>
+    </div>
+</template>
+
+<script>
+import Header from './Header.vue'
+import HeroAdd from '@/components/Hero-add.vue'
+import HeroDetail from './Hero-detail.vue'
+import HeroList from './Hero-list.vue'
+
+    export default {
+        name:'Heros',
+        components:{Header,HeroAdd,HeroDetail,HeroList}
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
