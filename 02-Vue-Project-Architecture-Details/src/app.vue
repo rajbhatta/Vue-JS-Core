95e15336a39ddc2abe9e067bc6273b58f66c0ead
Raj Bhatta <bhattaraj21@gmail.com>
2021-04-16 22:01:15 -0700

    Project structure details

---
+++ 02-Vue-Project-Architecture-Details/src/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <HeaderBar />
+    <div class="main-section content-title-group">
+      <h2 class="title">Heroes</h2>
+      <div>We'll start here</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderBar from '@/components/header-bar';
+
+export default {
+  name: 'App',
+  components: { HeaderBar },
+};
+</script>
+
+<style lang="scss">
+@import '@/design/index.scss';
+</style>
