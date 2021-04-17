1d7e71e02df80962d344167b82e2911102d36b05
Raj Bhatta <bhattaraj21@gmail.com>
2021-04-17 16:35:40 -0700

    Vue js data model update

--- 05-Interacting-within-component/01-Defining-datamodel/src/components/Heros.vue
+++ 05-Interacting-within-component/01-Defining-datamodel/src/components/Heros.vue
@@ -1,21 +1,17 @@
 <template>
     <div>
         <Header></Header>
-        <HeroAdd></HeroAdd>
-        <HeroDetail></HeroDetail>
         <HeroList></HeroList>
     </div>
 </template>
 
 <script>
 import Header from './Header.vue'
-import HeroAdd from '@/components/Hero-add.vue'
-import HeroDetail from './Hero-detail.vue'
 import HeroList from './Hero-list.vue'
 
     export default {
         name:'Heros',
-        components:{Header,HeroAdd,HeroDetail,HeroList}
+        components:{Header,HeroList}
     }
 </script>
 
