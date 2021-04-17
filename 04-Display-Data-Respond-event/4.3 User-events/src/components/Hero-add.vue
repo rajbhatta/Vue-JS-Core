ae0953194a4309d2cd0f73890d46d7281cc5ae0b
Raj Bhatta <bhattaraj21@gmail.com>
2021-04-17 13:26:38 -0700

    Event handling in VueJs

---
+++ 04-Display-Data-Respond-event/4.3 User-events/src/components/Hero-add.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h3>Add Hero </h3>
+
+        <form>
+        <label for="fname">First name:</label><br>
+        <input type="text" id="fname" name="fname"><br>
+
+        <label for="lname">Last name:</label><br>
+        <input type="text" id="lname" name="lname"><br><br>
+
+        <input type="button" value="Submit" v-on:click="submitForm"> &nbsp; &nbsp;
+         <input type="button" value="Clear" @click="clearForm">
+        </form>
+
+        <h1>Event {{message}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'HeroAdd',
+        data() {
+            return {
+                message: ""
+            }
+        },
+        methods: {
+            submitForm() {
+                this.message="Submit Form button is clicked";
+            },
+            clearForm(){
+                this.message="Clear Form button is clicked";
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
