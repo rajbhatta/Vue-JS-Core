e60b1bc290a2e5892912587c8047e339c2c57b91
Raj Bhatta <bhattaraj21@gmail.com>
2021-04-17 17:20:42 -0700

    Component lifecycle hook

---
+++ 05-Interacting-within-component/03-Component-lifecycle-hook/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Heros></Heros>
+</template>
+
+<script>
+import Heros from './components/Heros.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Heros
+  }
+}
+</script>
