d1bb7cee6579a030d9af791d3c19c097ee6f7344
Raj Bhatta <bhattaraj21@gmail.com>
2021-04-17 17:37:21 -0700

    Watching properties in VueJs

---
+++ 05-Interacting-within-component/04-Watching-properties/src/components/Hero-list.vue
@@ -0,0 +1,50 @@
+<template>
+    <h4>This is hero list</h4>
+    
+    <div v-for="hero in heros" :key="hero.id">
+        {{hero.firstName}}
+    </div>
+
+</template>
+
+<script>
+    const OUR_HEROES=[
+        {
+            id:10,
+            firstName:"AF",
+            lastName: "AL",
+            description:"Test A"
+        },
+        {
+            id:11,
+            firstName:"BF",
+            lastName: "BL",
+            description:"Test B"
+        },
+        {
+            id:12,
+            firstName:"CF",
+            lastName: "CL",
+            description:"Test C"
+        },
+        {
+            id:13,
+            firstName:"DF",
+            lastName: "DL",
+            description:"Test D"
+        }
+    ];
+
+    export default {
+        name:'HeroList',
+        data() {
+            return {
+                heros: OUR_HEROES
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
