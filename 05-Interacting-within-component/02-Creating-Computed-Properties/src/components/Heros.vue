2236d9b8e85cb2c8c8beece18374d0e9f9eea4b2
Raj Bhatta <bhattaraj21@gmail.com>
2021-04-17 17:02:04 -0700

    Working with computed properties in VueJs

---
+++ 05-Interacting-within-component/02-Creating-Computed-Properties/src/components/Heros.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Header></Header>
+        <HeroList></HeroList>
+    </div>
+</template>
+
+<script>
+import Header from './Header.vue'
+import HeroList from './Hero-list.vue'
+
+    export default {
+        name:'Heros',
+        components:{Header,HeroList}
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
