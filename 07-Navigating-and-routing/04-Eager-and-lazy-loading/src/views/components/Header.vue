c62b6536e11577d7b2ed9b872c5238efb5f53a02
Raj Bhatta <bhattaraj21@gmail.com>
2021-04-17 20:17:50 -0700

    Renamed

---
+++ 07-Navigating-and-routing/04-Eager-and-lazy-loading/src/views/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <a :href="github">This is GitHubLink </a>
+        <a v-bind:href="twitter">This is Twitter </a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Header',
+        data() {
+            return {
+                github: "http://github.com",
+                twitter:"http://twitter.com"
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
